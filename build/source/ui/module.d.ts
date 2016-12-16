faa2429949c26233742658d214706baf9384768b
Alexander Cooke <alex.cooke@live.com>
2016-12-16 14:59:19 +1000

    remove all components

--- build/source/ui/module.d.ts
+++ build/source/ui/module.d.ts
@@ -1,5 +1,1 @@
-import text from './input/text/component';
-import number from './input/number/component';
-import numberSpinner from './input/number-spinner/component';
-import percentage from './input/percentage/component';
-export { text, number, numberSpinner, percentage };
+export {  };
--- source/ui/module.ts
+++ source/ui/module.ts
@@ -1,9 +1,9 @@
 import * as ng from 'angular';
 
-import text from './input/text/component';
-import number from './input/number/component';
-import numberSpinner from './input/number-spinner/component';
-import percentage from './input/percentage/component';
+// import text from './input/text/component';
+// import number from './input/number/component';
+// import numberSpinner from './input/number-spinner/component';
+// import percentage from './input/percentage/component';
 
 //export default function ui(): ng.IModule {
 
@@ -13,16 +13,12 @@ let app = ng.module('ui-testing', [
 
 console.log("initializing ui-testing");
 
-text(app);
-number(app);
-numberSpinner(app);
-percentage(app);
+// text(app);
+// number(app);
+// numberSpinner(app);
+// percentage(app);
 
 export {
-    text,
-    number,
-    numberSpinner,
-    percentage
 };
 
   //  return app;
