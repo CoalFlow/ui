d14a13fa18b94855fca14012bdbc547ee2275f68
LennyThompson <LennyThompson@gmail.com>
2017-01-17 16:56:01 +1000

    Addint uints and factor to percentage components

--- source/ui/input/percentage-spinner/component.ts
+++ source/ui/input/percentage-spinner/component.ts
@@ -10,33 +10,58 @@ export interface IUiInputPercentageOptions extends IUiInputCommonOptions {
 
 export class UiInputPercentageSpinnerController extends UiInputNumberSpinnerController {
 
+    _factor: number;
+    invFactor: number;
+    units: string;
+
     constructor($element: ng.IAugmentedJQuery, $attrs: ng.IAttributes)
     {
         super($element, $attrs);
         this.min = 0;
         this.max = 100;
+        this._factor = 100;
+        this.invFactor = 1 / this._factor;
+        this.units = "%";
     }
 
+    get factor(): number
+    {
+        return this._factor;
+    }
+
+    set factor(value: number)
+    {
+        this._factor = +value;
+        if(this._factor)
+        {
+            this.invFactor = 1 / this._factor;
+        }
+    }
+
+    get invervseFactor(): number
+    {
+        return this.invFactor;
+    }
 
     incrementValue(): void {
-        this.changeValueBy(this.increment / 100);
+        this.changeValueBy(this.increment * this.invervseFactor);
     }
 
     decrementValue(): void {
-        this.changeValueBy((this.increment / 100) * -1);
+        this.changeValueBy((this.increment * this.invervseFactor) * -1);
     }
 
     parse(value): number {
         let parsedValue: number = super.parse(value);
         if (parsedValue != null) {
-            parsedValue /= 100;
+            parsedValue *= this.invervseFactor;
         }
         return parsedValue
     }
 
     format(value): string {
         if (value != null) {
-            return super.format(value * 100);
+            return super.format(value * this.factor);
         } else {
             return null;
         }
@@ -53,6 +78,8 @@ export class UiInputPercentageSpinnerComponent extends UiInputNumberSpinnerCompo
         this.controller = UiInputPercentageSpinnerController;
         this.template = require('./template.html');
 
+        this.bindings["units"] = "@?uiUnits";
+        this.bindings["factor"] = "@?uiFactor";
     }
 
 
--- source/ui/input/percentage/component.ts
+++ source/ui/input/percentage/component.ts
@@ -11,25 +11,50 @@ export interface IUiInputPercentageOptions extends IUiInputCommonOptions {
 
 export class UiInputPercentageController extends UiInputNumberController {
 
+    _factor: number;
+    invFactor: number;
+    units: string;
+
     constructor($element: ng.IAugmentedJQuery, $attrs: ng.IAttributes) {
         super($element, $attrs);
 
         this.min = 0;
         this.max = 100;
+        this._factor = 100;
+        this.invFactor = 1 / this._factor;
+        this.units = "%";
+    }
 
+    get factor(): number
+    {
+        return this._factor;
+    }
+
+    set factor(value: number)
+    {
+        this._factor = +value;
+        if(this._factor)
+        {
+            this.invFactor = 1 / this._factor;
+        }
+    }
+
+    get invervseFactor(): number
+    {
+        return this.invFactor;
     }
 
     parse(value): number {
         let parsedValue: number = super.parse(value);
         if (parsedValue != null) {
-            parsedValue /= 100;
+            parsedValue *= this.invervseFactor;
         }
         return parsedValue
     }
 
     format(value): string {
         if (value != null) {
-            return super.format(value * 100);
+            return super.format(value * this.factor);
         } else {
             return null;
         }
@@ -47,5 +72,7 @@ export class UiInputPercentageComponent extends UiInputNumberComponent
         this.controller = UiInputPercentageController;
         this.template = require('./template.html');
 
+        this.bindings["units"] = "@?uiUnits";
+        this.bindings["factor"] = "@?uiFactor";
     }
 }
